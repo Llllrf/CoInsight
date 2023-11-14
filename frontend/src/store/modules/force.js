2ec62bf52a45a98571a06e702e1805c3fc06d6cf
lrf <1269957412@qq.com>
2023-11-14 23:06:36 +0800

    use intranet server to run backend

--- frontend/src/store/modules/force.js
+++ frontend/src/store/modules/force.js
@@ -2,7 +2,8 @@ export default {
   namespaced: true,
   state() {
     return {
-      baseUrl: "http://127.0.0.1:5000",
+      // baseUrl: "http://127.0.0.1:5000", 
+      baseUrl: "http://10.1.114.103:5001",  // run backend in an intranet server
 
       // load state
       loading: false,
